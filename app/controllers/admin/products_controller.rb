6ba0a3d6c46ccdd2b15c5da6a14a6baa8e081b9e
sfia-o <sofia.vl.oliveira@gmail.com>
2023-10-25 14:03:35 -0700

    Implement admin security.

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -39,7 +39,7 @@ class Admin::ProductsController < ApplicationController
   end
 
   def authenticate_admin
-    authenticate do |username, password|
+    authenticate_or_request_with_http_basic do |username, password|
       username == ENV['BASIC_AUTH_USERNAME'] && password == ENV['BASIC_AUTH_PASSWORD']
     end
   end
