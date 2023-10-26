b9c69bdd636df756fa9dcb3b8be472fead2fed01
sfia-o <sofia.vl.oliveira@gmail.com>
2023-10-26 14:10:10 -0700

    Implement Controller actions.

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,38 @@
+class Admin::CategoriesControllerController < ApplicationController
+  before_action :authenticate_admin
+
+  def index
+    @categories = Category.order(id: :desc).all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(
+      :name,
+      :created_at,
+      :updated_at
+    )
+  end
+
+  def authenticate_admin
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV['BASIC_AUTH_USERNAME'] && password == ENV['BASIC_AUTH_PASSWORD']
+    end
+  end
+
+end
