2b879b6e1910f2c84a6905b834563745406d99b2
sfia-o <sofia.vl.oliveira@gmail.com>
2023-11-01 10:03:56 -0700

    Refactor

--- db/schema.rb
+++ db/schema.rb
@@ -57,7 +57,7 @@ ActiveRecord::Schema.define(version: 2023_10_25_173310) do
     t.string "first_name"
     t.string "last_name"
     t.string "email"
-    t.string "password"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
