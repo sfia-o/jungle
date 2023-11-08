aa2524cddb729aa02583d7c46a3f0c8fc8a4b1d6
sfia-o <sofia.vl.oliveira@gmail.com>
2023-11-07 16:39:38 -0800

    Add password match test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+    it 'requires a password and password confirmation match' do
+      user = User.new(
+        email: "test@example.com",
+        password: 'password1',
+        password_confirmation: 'password2'
+      )
+      expect(user).to_not be_valid
+    end
+
+    
+
+    end
 end
