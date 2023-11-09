1a308f1451688c018ca5d908cdf901be4a24568f
sfia-o <sofia.vl.oliveira@gmail.com>
2023-11-09 15:58:45 -0800

    Add test to navigate to product details

---
+++ cypress/integration/product_details.spec.js
@@ -0,0 +1,12 @@
+describe('visit product details page', () => {
+
+  beforeEach(() => {
+    cy.visit('http://localhost:3000')
+  });
+  
+  it('should navigate from homepage to product page', () => {
+    cy.get('.product').first().click();
+    
+  });
+
+})
